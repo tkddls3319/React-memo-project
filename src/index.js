3e6618e5c9477a23225f475e6be6f9c30e556afc
tkddls3319 <54829486+tkddls3319@users.noreply.github.com>
2025-02-24 22:31:55 +0900

    아코디언 컴포넌트 만들기
    아코디언 컴포넌트는 사라졌다 나왔다하는 컴포넌트

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CurseCard from './components/CourseCard';
-
+import Accordion from './components/Accordion';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <CurseCard
-      img="https://dst6jalxvbuf5.cloudfront.net/media/images/Course/cover_image/220208_122751/%EC%BD%94%EC%8A%A4%EC%B9%B4%EB%93%9CB_PC.png"
-      tags={['발표', '패키지', '최대할인']}
-      title="비즈니스 올인원, 방구석 어학연수 패키지"
-      startPrice={390000}
-      types={['동영상강의']}
-    />
-  </React.StrictMode>,
+  <div style={{ fontsize: '2rem', padding: 30 }}>
+    <Accordion title="This is a Title" content="This is a content" />
+  </div>,
 );
 
 // If you want to start measuring performance in your app, pass a function
