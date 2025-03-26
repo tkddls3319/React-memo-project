0c9a01b4c2b47006d2becb93288c6bc5d9e2e344
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2025-03-26 16:34:53 +0900

    localStorage로 데이터 보관방법 lodash.debounce 라이브러리 최적화, useCallback최적화
    Cookie
    네트워크 요청시 서버로 함께 저장되는 문자열 데이터이다. 보통 사용자의 활도기록을 저장해 놨다가 서버로 전달할 때 사용한다.
    하지만 쿠키가 많아지거나 네트워크 요청이 많아지면 효율성이 떨어진다. 그래서 html5에는 랩 스토리지라는 Local Storage, Session Storage가 만들어진다.
    
    Local Storage
    Key, Value 쌍으로 데이터를 저장하고 조회할 수 있는 데이터 저장소로 서버로 전달되지 않는다.
    지우지 않는 이상 브라우저나 탭을 닫았다 열어도 계속 유지된다.
    
    Session Storage
    Key, Value 쌍으로데 데이터를 저장하고 조회할 수 있는 데이터 저장소로 서버로 전달되지 않는다.
    같은 탭 안에서는 데이터가 유지되지만 탭을 닫거나 브라우저를 닫으면 데이터가 지워진다.
    
    2. lodash.debounce 라이브러리 사용
    (모든 타이핑을 저장하지 말고 한번에 묶어 저장하는 방법
    
    3. useCallback최적화

--- src/App.js
+++ src/App.js
@@ -1,56 +1,70 @@
 import './App.css';
 import MemoContainer from './components/MemoContainer';
 import SideBar from './components/SideBar';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { setItem, getItem } from './lib/storage';
+import debounce from 'lodash.debounce';
+
+const debouncedSetItem = debounce(setItem, 5000); //setItem을 5초에 한번만 저장, 바로 setItem 함수를 사용하는게아니라 debounce에서 리턴한 debouncedSetItem를 사용해야 적용됨.
 
 function App() {
-  const [memos, setMemos] = useState([
-    {
-      title: 'Memo 1',
-      content: 'this is memo1',
-      createdAt: 1740459712601,
-      updatedAt: 1740459712601,
-    },
-    {
-      title: 'Memo 2',
-      content: 'this is memo2',
-      createdAt: 1740459743113,
-      updatedAt: 1740459743113,
-    },
-  ]);
+  const [memos, setMemos] = useState(getItem('memo') || []);
 
   const [selectedMemoIndex, setSelectedMemoIndex] = useState(0);
 
-  const setMemo = (newMemo) => {
-    const newMemos = [...memos]; //useState으로 관리하는 객체는 불변성을 유지해야함 -> 새로운 객체 생성
-    newMemos[selectedMemoIndex] = newMemo;
-    setMemos(newMemos); //새로운 객체 생성
-  };
+  const setMemo = useCallback(
+    (newMemo) => {
+      setMemos((memos) => {
+        //새로운 객체 생성
+        const newMemos = [...memos]; //useState으로 관리하는 객체는 불변성을 유지해야함 -> 새로운 객체 생성
+        newMemos[selectedMemoIndex] = newMemo;
+        debouncedSetItem('memo', newMemos);
+        return newMemos;
+      });
+      // localStorage.setItem('memo', JSON.stringify(newMemos));
+    },
+    [selectedMemoIndex],
+  );
 
-  const addMemo = () => {
-    const now = new Date().getTime();
-    setMemos([
-      ...memos,
-      {
-        title: 'Untitled',
-        content: '',
-        createdAt: now,
-        updatedAt: now,
-      },
-    ]);
+  const addMemo = useCallback(() => {
+    setMemos((memos) => {
+      const now = new Date().getTime();
+
+      const newMemos = [
+        ...memos,
+        {
+          title: 'Untitled',
+          content: '',
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+
+      // localStorage.setItem('memo', JSON.stringify(newMemos));
+      debouncedSetItem('memo', newMemos);
+
+      return newMemos;
+    });
     setSelectedMemoIndex(memos.length);
-  };
+  }, [memos]);
 
-  const deleteMemo = (index) => {
-    const newMemos = [...memos];
+  const deleteMemo = useCallback(
+    (index) => {
+      setMemos((memos) => {
+        const newMemos = [...memos];
 
-    newMemos.splice(index, 1);
+        newMemos.splice(index, 1);
+        debouncedSetItem('memo', newMemos);
+        // localStorage.setItem('memo', JSON.stringify(newMemos));
 
-    setMemos(newMemos);
-    if (index == selectedMemoIndex) {
-      setSelectedMemoIndex(0);
-    }
-  };
+        return newMemos;
+      });
+      if (index === selectedMemoIndex) {
+        setSelectedMemoIndex(0);
+      }
+    },
+    [selectedMemoIndex],
+  );
 
   return (
     <div className="App">
